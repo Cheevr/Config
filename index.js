140e73c2dda73082cdf1ed5bddb29581ce7b80b3
Ravi Gairola <mallox@pyxzl.net>
2016-10-14 14:00:22 -0700

    First working version

---
+++ index.js
@@ -0,0 +1,126 @@
+var args = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+var path = require('path');
+
+
+/**
+ * Configurations will be loaded based on the prefix given through the tier, meaning that tier d would resolve to
+ * the file that starts with d (excluding the default and override files).
+ * Default files can be specified using command line arguments, environment variables, or using the reload functionality:
+ *
+ * Arguments will be parsed longest to shortest so that you can override shorter arguments to your program if you want.
+ * This means that you can call your program with the flag '-d config/' or '--directory config/ -d myopt' and it will
+ * still function while you use your own arguments.
+ *
+ * Environment variables will be read from process.env and can be set however your systems sets up variables.
+ *
+ * Options:
+ * directory:   NODE_CONF_DIR               -d --dir --directory    The directory from which to read config files
+ * default:     NODE_CONF_DEFAULT           -s --default            The filename prefix for the default config
+ * override:    NODE_CONF_OVERRIDE          -o --override           The filename prefix for the override config
+ * tier:        NODE_CONF_TIER, NODE_ENV    -t --tier               The tier for which to load the config
+ *
+ */
+class Config {
+    /**
+     * Loads the configurations from the file system and makes them available on the object.
+     * @param {string} tier
+     * @private
+     */
+    _load(tier) {
+        try {
+            let files = fs.readdirSync(this.dir);
+            let defaultConfig = {};
+            let tierConfig = {};
+            let overrideConfig = {};
+            for (let file of files) {
+                if (file.startsWith(this.default)) {
+                    defaultConfig = require(path.join(this.dir, file));
+                    this._tier = this._tier || path.parse(file).name;
+                } else if (file.startsWith(this.override)) {
+                    overrideConfig = require(path.join(this.dir, file));
+                } else if (file.startsWith(tier)) {
+                    tierConfig = require(path.join(this.dir, file));
+                    this._tier = path.parse(file).name;
+                }
+            }
+            this.__proto__.__proto__ = Object.assign({}, defaultConfig, tierConfig, overrideConfig);
+        } catch(e) {
+            this._tier = tier;
+        }
+    }
+
+    /**
+     * @returns {string} the currently set tier
+     */
+    get tier() {
+        if (this._tier) {
+            return this._tier;
+        }
+        let tier = process.env.NODE_CONF_TIER || process.env.NODE_ENV || 'development';
+        tier = args.t || args.tier || tier;
+        this.tier = tier;
+        return this.tier;
+    }
+
+    /**
+     * Set the tier and automatically reloads the configuration
+     * @param {string} tier
+     */
+    set tier(tier) {
+        this._load(tier);
+    }
+
+    /**
+     * @returns {string} The directory in which all configurations are stored.
+     */
+    get dir() {
+        if (this._dir) {
+            return this._dir;
+        }
+        this._dir = process.env.NODE_CONF_DIR || path.join(require.main.filename, 'config');
+        this._dir = args.directory || args.dir || args.d || this._dir;
+        return this._dir;
+    }
+
+    /**
+     * Returns the name/prefix used for the override file.
+     * @returns {string}
+     */
+    get override() {
+        if (this._override) {
+            return this._override;
+        }
+        this._override = process.env.NODE_CONF_OVERRIDE || false;
+        this._override = args.override || args.o || this._override;
+        return this._override;
+    }
+
+    /**
+     * Returns the name/prefix for the default file.
+     * @returns {*}
+     */
+    get default() {
+        if (this._default) {
+            return this._default;
+        }
+        this._default = process.env.NODE_CONF_DEFAULT || 'default.';
+        this._default = args.s || args.default || this._default;
+        return this._default;
+    }
+
+    /**
+     * Will reload the configuration system
+     * @param {string} [tier]       The new tier the config should be loaded for
+     * @param {string} [dir]        The directory from which the config should be loaded
+     * @param {string} [override]   An additional configuration that will override tier and default config if set.
+     */
+    reload(tier = this.tier, dir = this._dir, override) {
+        delete this._tier;
+        this._dir = path.isAbsolute(dir) ? dir : path.join(require.main.filename, dir);
+        this._override = override;
+        this.tier = tier;
+    }
+}
+
+module.exports = new Config();
---
+++ test/config.test.js
@@ -0,0 +1,58 @@
+var config = require('..');
+var expect = require('chai').expect;
+var path = require('path');
+
+describe('tiers', () => {
+    it('should set the tier to development by default', () => {
+        config.reload();
+        expect(config.tier).to.equal('development');
+        expect(config.dir).to.equal(path.join(require.main.filename, 'config'));
+    });
+
+    it('should reload the configuration from a new directory', () => {
+        config.reload('prod', 'testconfig');
+        expect(config.tier).to.equal('prod');
+        expect(config.dir).to.equal(path.join(require.main.filename, 'testconfig'));
+    });
+
+    it('should load the default configuration', () => {
+        config.reload('none', path.join(__dirname, 'data'));
+        expect(config.tier).to.equal('default');
+        expect(config.prop).to.equal(1);
+        expect(config.devprop).to.be.not.ok;
+        expect(config.stagingprop).to.be.not.ok;
+    });
+
+    it('should load the development config', () => {
+        config.reload('d', path.join(__dirname, 'data'));
+        expect(config.tier).to.equal('development');
+        expect(config.prop).to.equal(2);
+        expect(config.devprop).to.be.ok;
+        expect(config.stagingprop).to.be.not.ok;
+    });
+
+    it('should load the staging config', () => {
+        config.reload('s', path.join(__dirname, 'data'));
+        expect(config.tier).to.equal('staging');
+        expect(config.prop).to.equal(3);
+        expect(config.devprop).to.be.not.ok;
+        expect(config.stagingprop).to.be.ok;
+    });
+
+    it('should override the default config', () => {
+        config.reload('none', path.join(__dirname, 'data'), 'local');
+        expect(config.tier).to.equal('default');
+        expect(config.override).to.be.ok;
+        expect(config.prop).to.equal(4);
+        expect(config.devprop).to.be.not.ok;
+        expect(config.stagingprop).to.be.not.ok;
+    });
+
+    it('should override a tier config', () => {
+        config.reload('d', path.join(__dirname, 'data'), 'l');
+        expect(config.tier).to.equal('development');
+        expect(config.prop).to.equal(4);
+        expect(config.devprop).to.be.ok;
+        expect(config.stagingprop).to.be.not.ok;
+    });
+});
